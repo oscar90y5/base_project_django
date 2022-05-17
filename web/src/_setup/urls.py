a27d31ff020d2028ddcb87313f530df370045a7f
Óscar Fernández Angulo <oscarfernandezangulo@gmail.com>
2022-05-17 20:33:24 +0200

    Actualizado el urls a la forma de Django 4.

--- web/src/_setup/urls.py
+++ web/src/_setup/urls.py
@@ -17,10 +17,9 @@ Including another URLconf
 from django.contrib import (
     admin,
 )
-from django.conf.urls import (
-    url,
-)
+from django.urls import path, include
+
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
